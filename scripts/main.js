70aeb09bb403f6d9b621d2928ee7cd9d6ee560f5
JackyCSYHX666 <60687457+JackyCSYHX666@users.noreply.github.com>
2020-02-06 15:37:07 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc ==='images / WZWY_HX.jpg'){
+      myImage.setAttribute('src','images / HX.jpg');
+    }else{
+      myImage.setAttribute('src','images / WZWY_HX.jpg');
+        }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = '国士无双:韓信, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = '国士无双:韓信, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
